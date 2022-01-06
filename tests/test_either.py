25cf7c525dd1169589ef7e05e322ae1622b0cf63
Luca Ruzzola <lucaruzzola@gmail.com>
2022-01-06 22:08:56 +0100

    improve Either tests

--- tests/test_either.py
+++ tests/test_either.py
@@ -1,37 +1,6 @@
 from pyfunds.either import Left, Right
 
 
-def test_equals_right():
-    initial_value = 5
-    r = Right(initial_value)
-    assert r == Right(initial_value) and Right(initial_value) == r
-
-
-def test_equals_left():
-    initial_value = 5
-    l = Left(initial_value)
-    assert l == Left(initial_value) and Left(initial_value) == l
-
-
-def test_not_equals():
-    initial_value = 5
-    other_value = 10
-    r = Right(initial_value)
-    r2 = Right(other_value)
-    l = Left(initial_value)
-    l2 = Left(other_value)
-    assert (
-        r != r2
-        and r2 != r
-        and r != l
-        and l != r
-        and r != l2
-        and l2 != r
-        and l != l2
-        and l2 != l
-    )
-
-
 def test_map_right():
     # GIVEN: an instance of Either that is Right with an initial value
     initial_value = 5
@@ -92,26 +61,6 @@ def test_flatmap_left_to_left():
     assert result == Left(initial_value)
 
 
-def test_swap_to_left():
-    # GIVEN: an instance of Either that is Right with an initial value
-    initial_value = 5
-    r = Right(initial_value)
-    # WHEN: we swap Left and Right
-    swapped = r.swap()
-    # THEN: the swapped result is Left and contains the initial value
-    assert swapped == Left(initial_value)
-
-
-def test_swap_to_right():
-    # GIVEN: an instance of Either that is Left with an initial value
-    initial_value = 5
-    r = Left(initial_value)
-    # WHEN: we swap Left and Right
-    swapped = r.swap()
-    # THEN: the swapped result is Right and contains the initial value
-    assert swapped == Right(initial_value)
-
-
 def test_fold_from_right():
     # GIVEN: an instance of Either that is Right with an initial value
     initial_value = 5
@@ -138,3 +87,63 @@ def test_fold_from_left():
     result = r.fold(fl, fr)
     # THEN: the result is the correct value and the correct function has been called
     assert result == fl(initial_value)
+
+
+def test_swap_to_left():
+    # GIVEN: an instance of Either that is Right with an initial value
+    initial_value = 5
+    r = Right(initial_value)
+    # WHEN: it's swapped
+    swapped = r.swap()
+    # THEN: the swapped result is Left and contains the initial value
+    assert swapped == Left(initial_value)
+
+
+def test_swap_to_right():
+    # GIVEN: an instance of Either that is Left with an initial value
+    initial_value = 5
+    r = Left(initial_value)
+    # WHEN: it's swapped
+    swapped = r.swap()
+    # THEN: the swapped result is Right and contains the initial value
+    assert swapped == Right(initial_value)
+
+
+def test_equals_right():
+    # GIVEN: an instance of Either that is Right with an initial value
+    initial_value = 5
+    r = Right(initial_value)
+    # WHEN: equality between two instances or Right with the same value is checked
+    # THEN: the equality holds both ways
+    assert r == Right(initial_value) and Right(initial_value) == r
+
+
+def test_equals_left():
+    # GIVEN: an instance of Either that is Left with an initial value
+    initial_value = 5
+    l = Left(initial_value)
+    # WHEN: equality between two instances or Left with the same value is checked
+    # THEN: the equality holds both ways
+    assert l == Left(initial_value) and Left(initial_value) == l
+
+
+def test_not_equals():
+    # GIVEN: different instances of Left and Right, with 2 possible values
+    initial_value = 5
+    other_value = 10
+    r = Right(initial_value)
+    r2 = Right(other_value)
+    l = Left(initial_value)
+    l2 = Left(other_value)
+    # WHEN: they are compared for equality
+    # THEN: none of the equalities holds
+    assert (
+        r != r2
+        and r2 != r
+        and r != l
+        and l != r
+        and r != l2
+        and l2 != r
+        and l != l2
+        and l2 != l
+    )
